9f0d33caa79e24fd7f07b4325d3d887939818c48
Lux Barker <lux.barker13@gmail.com>
2022-12-06 17:30:50 -0800

    solve day 3 part 2

--- 03/02/main.go
+++ 03/02/main.go
@@ -1,1 +1,48 @@
-package _2
+package main
+
+import (
+	"bufio"
+	"fmt"
+	mapset "github.com/deckarep/golang-set/v2"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(f)
+	var groupPos int
+	var points int
+	sets := []mapset.Set[string]{mapset.NewSet[string](), mapset.NewSet[string]()}
+
+	// read through one line at a time
+	for scanner.Scan() {
+		// grab the text of the current line
+		currLine := scanner.Text()
+		// are we on the first or second elf of the group?
+		if groupPos < 2 {
+			for _, s := range currLine {
+				sets[groupPos].Add(string(s))
+			}
+			groupPos++
+		} else {
+			// we are on the last elf of the group
+			intersection := sets[0].Intersect(sets[1])
+			for _, s := range currLine {
+				if intersection.Contains(string(s)) {
+					if s >= 97 {
+						points += int(s) - 96
+					} else {
+						points += int(s) - 38
+					}
+					break
+				}
+			}
+			groupPos = 0
+			sets = []mapset.Set[string]{mapset.NewSet[string](), mapset.NewSet[string]()}
+		}
+	}
+	fmt.Println(points)
+}
